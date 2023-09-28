a6eeb27720ce1dcc8d3af133b2fd542d62ccd008
yigitkader <ryigitkader@gmail.com>
2023-09-28 21:51:08 +0200

    enemy animations added, weapon added, scene edit

--- Assets/Scripts/Dreyar/DreyarModel.cs
+++ Assets/Scripts/Dreyar/DreyarModel.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GladiatorModel{
-    
-    public const float MOVEMENT_SPEED = 2f;
+public class DreyarModel : MonoBehaviour
+{
+    public const float MOVEMENT_SPEED = 1f;
     public const float GRAVITY = 9.8f;
   
-    public const float ROTATION_SPEED = 0.15f;
+    public const float ROTATION_SPEED = 0.14f;
 
     public const float ROTATION_DEGREES_PER_SECOND= 180f;
 
