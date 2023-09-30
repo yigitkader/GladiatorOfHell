1e2d810703c88cf383b976ba6cec8fddfe448cff
yigitkader <ryigitkader@gmail.com>
2023-10-01 01:14:25 +0200

    first enemy added, chasing and attacking added

---
+++ Assets/Scripts/PleasantMan/PleasantManController.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public enum PleasantManState{
+    CHASE,
+    ATTACK
+}
+public class PleasantManController : MonoBehaviour{
+    
+    private PleasantManAnimations pleasantManAnimations;
+    private NavMeshAgent navMeshAgent;
+
+    private Transform gladiatorTarget;
+
+    [SerializeField]
+    private float movementSpeed =  PleasantManModel.MOVEMENT_SPEED;
+
+    [SerializeField]
+    private float attackDistance = PleasantManModel.ATTACK_DISTANCE;
+
+    [SerializeField]
+    private float chaseGladiatorAfterAttackDistance = PleasantManModel.CHASE_GLADIATOR_AFTER_ATTACK_DISTANCE;
+
+    [SerializeField]
+    private float waitBeforeAttackTime = PleasantManModel.WAIT_BEFORE_ATTACK_TIME;
+
+    private float attackTimer ;
+
+
+    private PleasantManState pleasantManState;
+
+
+    private void Awake() {
+        Debug.Log(Tags.GLADIATOR_TAG);
+        pleasantManAnimations = GetComponent<PleasantManAnimations>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        gladiatorTarget = GameObject.FindGameObjectWithTag(Tags.GLADIATOR_TAG).transform;
+    }
+
+    private void Start() {
+        pleasantManState = PleasantManState.CHASE;
+
+        attackTimer = waitBeforeAttackTime;
+    }
+
+    private void Update() {
+
+        if(pleasantManState == PleasantManState.CHASE){
+            ChaseGladiator();
+        }
+
+        if(pleasantManState == PleasantManState.ATTACK){
+            AttackToGladiator();
+        }
+        
+    }
+
+
+    private void ChaseGladiator(){
+        navMeshAgent.SetDestination(gladiatorTarget.position);
+        navMeshAgent.speed = movementSpeed;
+
+        if(navMeshAgent.velocity.sqrMagnitude == 0){
+            pleasantManAnimations.Run(false);
+        }else{
+            pleasantManAnimations.Run(true);
+        }
+
+
+        if(Vector3.Distance(transform.position,gladiatorTarget.position) <= attackDistance){
+            pleasantManState = PleasantManState.ATTACK;
+        }
+    }
+
+    private void AttackToGladiator(){
+        navMeshAgent.velocity = Vector3.zero;
+        navMeshAgent.isStopped = true;
+        pleasantManAnimations.Run(false);
+
+        attackTimer += Time.deltaTime;
+        if(attackTimer > waitBeforeAttackTime){
+            pleasantManAnimations.RandomAttack();
+            attackTimer = 0f;
+        }
+
+        if(Vector3.Distance(transform.position, gladiatorTarget.position) > attackDistance + chaseGladiatorAfterAttackDistance){
+            navMeshAgent.isStopped = false;
+            pleasantManState = PleasantManState.CHASE;
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+} // endClass
---
+++ Assets/Scripts/PleasantMan/PleasantManModel.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PleasantManModel : MonoBehaviour
+{
+    
+    public const float MOVEMENT_SPEED = 2f;
+
+    public const float ATTACK_DISTANCE = 2f;
+
+    public const float CHASE_GLADIATOR_AFTER_ATTACK_DISTANCE = 1f;
+
+    public const float WAIT_BEFORE_ATTACK_TIME = 3f;
+
+    //public const float ATTACK_TIMER = 
+
+
+
+    public const float GRAVITY = 9.8f;
+  
+    public const float ROTATION_SPEED = 0.15f;
+
+    public const float ROTATION_DEGREES_PER_SECOND= 180f;
+}
