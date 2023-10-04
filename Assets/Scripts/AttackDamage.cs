4c9305bcad19d362d7781e3bbdbbabf831e310ae
yigitkader <ryigitkader@gmail.com>
2023-10-05 00:33:00 +0200

    added attack points, and custom layers only for parents

---
+++ Assets/Scripts/AttackDamage.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackDamage : MonoBehaviour
+{
+    public LayerMask layerMask;
+    public float radius = 1f;
+
+    public float damage = 1f;
+    
+}
